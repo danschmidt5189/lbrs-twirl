59888d93860a26cdfd2698cbb589d38a8740c15e
Daniel Schmidt <danschmidt5189@gmail.com>
2014-04-08 16:43:01 -0700

    Fixes Twirl symlink

--- libs/twirl.pm
+++ libs/twirl.pm
@@ -1,1 +1,27 @@
-../openshift/lib/openshift.pm
+#
+#  Twirl.pm Application Class File
+#
+package Twirl;
+
+use Dancer ':syntax';
+use API;
+
+our $VERSION = '0.1';
+
+# Content-type negotiation
+set serializer => 'Mutable';
+
+# Home page template
+get '/home' => sub {
+	template 'home';
+};
+
+# API Method 1: Returns most recent tweets from a user
+get '/tweets/:username' => sub {
+	return Twirl::API::RecentTweets( params->{username} );
+};
+
+# API Method 2: Returns list of friends that two users have in common
+get '/friends/:username_1/:username_2' => sub {
+	return Twirl::API::SharedFriends( params->{username_1}, params->{username_2} );
+};
